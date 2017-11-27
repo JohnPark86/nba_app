ebef29ba5ac8937b6358eea0a6345d8cc10bc215
John <jduncanpark@gmail.com>
2017-11-26 23:16:20 -0500

    mapStateToProps stuff

--- assets/js/components/Info.js
+++ assets/js/components/Info.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import store from '../redux/store';
+import { connect } from 'react-redux';
 var NBA = require("nba");
 
-let player;
-store.subscribe( () => {
-		player = store.getState().searchReducer;
-		console.log("test1: ", player)
-});
+// let player;
+// store.subscribe( () => {
+// 		player = store.getState().searchReducer;
+// 		console.log("test1: ", player)
+// });
 
 //const curry = NBA.findPlayer(player);
 
-export default class Info extends React.Component {
+class Info extends React.Component {
 	
+	constructor(props) {
+    	super(props);
+	}
+
 	render(){
 		return(
-			<h2>{player}</h2>
+			<h2>{this.props.player}</h2>
 		)
 	}
 }
+
+
+function mapStateToProps(state){
+	console.log("mapStateToProps")
+	return {
+    	player: state.searchReducer
+	}
+}
+
+export default connect(mapStateToProps,null)(Info);
--- assets/js/components/Input.js
+++ assets/js/components/Input.js
@@ -1,39 +1,48 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import store from '../redux/store';
+import { connect } from 'react-redux';
 import { addSearchParam } from '../redux/actions';
 
 
-export default class Input extends React.Component {
+class Input extends React.Component {
 
 	constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {player: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({player: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    store.dispatch(addSearchParam(this.state.value))
+    store.dispatch(addSearchParam(this.state.player))
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.player} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
     );
   }
 }
+
+function mapStateToProps(state){
+	console.log(state)
+	return {
+    	player: state.searchReducer
+	}
+}
+
+export default connect(mapStateToProps,null)(Input);
 
 
 
